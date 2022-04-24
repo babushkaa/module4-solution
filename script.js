8c71fec0828f7a974f030b689438c5455f90e848
babushkaa <58389154+babushkaa@users.noreply.github.com>
2022-04-24 17:21:23 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,13 +1,10 @@
 
 (function () {
 
-var names = ["Juju", "Jeje", "Jojo", "Pipi", "Pupu", "Pepe"];
-
+var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 for (var i = 0; i < names.length; i++) {
 
-
   var firstLetter = names[i].charAt(0).toLowerCase();
-
 
   if (firstLetter === 'j') {
     byeSpeaker.speak(names[i]);
